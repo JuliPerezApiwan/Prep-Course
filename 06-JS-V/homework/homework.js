32c5a0c66c0883226beb2e9607a9eff58eac3cbc
Julieta Perez Apiwan <julieta.perezapiwan@gmail.com>
2022-05-17 23:01:42 -0300

    Tarea Resuelta

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -40,10 +40,8 @@ function agregarStringInvertida() {
   // Pista: Necesitarás usar "this" dentro de "reverse"
   String.prototype.reverse = function() {
     var stringInvertida = '';
-    for(var i = this.length - 1; i>=0; i--) {
-      stringInvertida = stringInvertida + this.charAt(i);
-    }
-    return stringInvertida;
+    return stringInvertida.split("").reverse().join("");
+    
   }
 }
 
